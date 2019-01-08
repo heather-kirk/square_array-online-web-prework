5b51b10c2a004817a37325e8f27bcde3d14fb6d2
Heather Kirk <hskirk@gmail.com>
2019-01-08 01:28:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
   numbers = [1,2,3]
-  square_array(numbers).each {|x| ** 2} = new_numbers
+  square_array(numbers).each ** 2 = new_numbers
   new _numbers = [9,10,16,25]
 end
